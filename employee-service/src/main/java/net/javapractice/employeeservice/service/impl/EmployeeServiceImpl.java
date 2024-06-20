7931ebfb2a0d074c6829dc26ee0a0b4e0add2a85
Vadim Shupan <shupan.vadimka@mail.ru>
2024-06-20 13:18:30 +0300

    added mapping

--- employee-service/src/main/java/net/javapractice/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javapractice/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javapractice.employeeservice.service.impl;
 import lombok.AllArgsConstructor;
 import net.javapractice.employeeservice.dto.EmployeeDto;
 import net.javapractice.employeeservice.entity.Employee;
+import net.javapractice.employeeservice.mapper.AutoEmployeeMapper;
 import net.javapractice.employeeservice.repository.EmployeeRepository;
 import net.javapractice.employeeservice.service.EmployeeService;
 import org.springframework.stereotype.Service;
@@ -16,21 +17,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     public EmployeeDto saveEmployee(EmployeeDto employeeDto){
 
         // convert employee dto to employee jpa entity
-        Employee employee = new Employee(
-                employeeDto.getId(),
-                employeeDto.getFirstName(),
-                employeeDto.getLastName(),
-                employeeDto.getEmail()
-        );
+        Employee employee = AutoEmployeeMapper.MAPPER.mapToEmployee(employeeDto);
 
         Employee savedEmployee = employeeRepository.save(employee);
 
-        EmployeeDto savedEmployeeDto = new EmployeeDto(
-                savedEmployee.getId(),
-                savedEmployee.getFirstName(),
-                savedEmployee.getLastName(),
-                savedEmployee.getEmail()
-        );
+        EmployeeDto savedEmployeeDto = AutoEmployeeMapper.MAPPER.mapToEmployeeDto(savedEmployee);
 
         return savedEmployeeDto;
     }
@@ -39,14 +30,6 @@ public class EmployeeServiceImpl implements EmployeeService {
     public EmployeeDto getEmployeeById(Long employeeId) {
 
         Employee employee = employeeRepository.findById(employeeId).get();
-
-        EmployeeDto employeeDto = new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail()
-        );
-
-        return employeeDto;
+        return AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
     }
 }
